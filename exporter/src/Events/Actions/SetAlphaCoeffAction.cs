57f8c69a4bb64336d073ba4a1621edaaa4708cd7
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-07-07 02:20:59 -0500

    Create SetAlphaCoeffAction.cs

---
+++ exporter/src/Events/Actions/SetAlphaCoeffAction.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using CTFAK.CCN.Chunks.Frame;
+using CTFAK.MMFParser.EXE.Loaders.Events.Parameters;
+
+public class SetAlphaCoeffAction : ActionBase
+{
+	public override int ObjectType { get; set; } = 2;
+	public override int Num { get; set; } = 65;
+
+	public override string Build(EventBase eventBase, Dictionary<string, string>? parameters = null, string ifStatement = "if (", string nextLabel = "")
+	{
+		StringBuilder result = new StringBuilder();
+
+		result.AppendLine($"for (ObjectIterator it(*{GetSelector(eventBase.ObjectInfo)}); !it.end(); ++it) {{");
+		result.AppendLine($"    auto instance = *it;");
+		result.AppendLine($"    instance->OI->BlendCoefficient = {ExpressionConverter.ConvertExpression((ExpressionParameter)eventBase.Items[0].Loader, eventBase)};");
+		result.AppendLine("}");
+
+		return result.ToString();
+	}
+}
