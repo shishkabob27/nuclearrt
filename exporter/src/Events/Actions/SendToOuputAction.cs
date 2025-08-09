11f48f5528d3b297aa28cfc8b964e77ac6a3ff00
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-08-09 18:23:54 -0500

    Create SendToOuputAction.cs

---
+++ exporter/src/Events/Actions/SendToOuputAction.cs
@@ -0,0 +1,20 @@
+using System.Text;
+using CTFAK.CCN.Chunks.Frame;
+using CTFAK.MMFParser.EXE.Loaders.Events.Parameters;
+
+public class SendToOutputAction : ActionBase
+{
+	public override int ObjectType { get; set; } = -1;
+	public override int Num { get; set; } = 42;
+
+	public override string Build(EventBase eventBase, ref string nextLabel, ref int orIndex, Dictionary<string, object>? parameters = null, string ifStatement = "if (")
+	{
+		StringBuilder result = new StringBuilder();
+
+		result.AppendLine("#ifdef _DEBUG");
+		result.AppendLine($"	std::cout << {ExpressionConverter.ConvertExpression((ExpressionParameter)eventBase.Items[0].Loader, eventBase)} << std::endl;");
+		result.AppendLine("#endif");
+		
+		return result.ToString();
+	}
+}
