8ddf5a923e45a6be5b30b2ddd0484bc10cba7a5d
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-11-08 20:59:25 -0600

    Create CompareMovementSpeed.cs

---
+++ exporter/src/Events/Conditions/CompareMovementSpeed.cs
@@ -0,0 +1,23 @@
+using System.Text;
+using CTFAK.CCN.Chunks.Frame;
+using CTFAK.MMFParser.EXE.Loaders.Events.Parameters;
+
+public class CompareMovementSpeedCondition : ConditionBase
+{
+	public override int ObjectType { get; set; } = 2;
+	public override int Num { get; set; } = -15;
+
+	public override string Build(EventBase eventBase, ref string nextLabel, ref int orIndex, Dictionary<string, object>? parameters = null, string ifStatement = "if (")
+	{
+		StringBuilder result = new();
+
+		result.AppendLine($"for (ObjectIterator it(*{GetSelector(eventBase.ObjectInfo)}); !it.end(); ++it) {{");
+		result.AppendLine($"    auto instance = *it;");
+		result.AppendLine($"    {ifStatement} (({ExpressionConverter.GetObjectClassName(eventBase.ObjectInfo, IsGlobal)}*)instance)->movements.GetCurrentMovement()->GetRealSpeed() {ExpressionConverter.GetComparisonSymbol(((ExpressionParameter)eventBase.Items[0].Loader).Comparsion)} {ExpressionConverter.ConvertExpression((ExpressionParameter)eventBase.Items[0].Loader, eventBase)}) it.deselect();");
+		result.AppendLine("}");
+
+		result.AppendLine($"if ({GetSelector(eventBase.ObjectInfo)}->Count() == 0) goto {nextLabel};");
+
+		return result.ToString();
+	}
+}
