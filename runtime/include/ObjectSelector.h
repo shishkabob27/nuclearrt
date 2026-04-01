7c6815bca9455f1477aca73fce38e612010f2053
programminglaboratorys <programming.laboratorys@gmail.com>
2026-04-01 10:51:47 +0300

    perf: optimized ObjectSelector to be lazy and swap and pop for deletion

--- runtime/include/ObjectSelector.h
+++ runtime/include/ObjectSelector.h
@@ -2,9 +2,7 @@
 
 #include "ObjectInstance.h"
 #include <vector>
-#include <memory>
 #include <algorithm>
-#include <iterator>
 
 class ObjectIterator;
 
@@ -12,14 +10,10 @@ class ObjectSelector {
 public:
 	using iterator = std::vector<ObjectInstance*>::iterator;
 	using const_iterator = std::vector<ObjectInstance*>::const_iterator;
-	using value_type = ObjectInstance*;
-	using reference = ObjectInstance*&;
-	using const_reference = const ObjectInstance*&;
-	using size_type = size_t;
-	
+
 	// Constructor that references an external collection
-	ObjectSelector(const std::unordered_map<unsigned int, ObjectInstance*>& allInstances, unsigned int objectInfoId, bool isQualifier = false) // used after object creation
-		: AllInstances(allInstances), ObjectInfoId(objectInfoId), IsQualifier(isQualifier) {
+	ObjectSelector(const std::unordered_map<unsigned int, ObjectInstance*>& allInstances, unsigned int objectInfoId, bool isQualifier = false)
+		: AllInstances(allInstances), ObjectInfoId(objectInfoId), IsQualifier(isQualifier), IsDirty(false) {
 		Initialize(allInstances);
 	}
 
@@ -27,57 +21,65 @@ public:
 	void Initialize(const std::unordered_map<unsigned int, ObjectInstance*>& allInstances) {
 		AllSelectorObjectInstances.clear();
 		for (const auto& [handle, instance] : allInstances) {
+			instance->isSelected = false; 
 			if (IsQualifier) {
 				for (int i = 0; i < 8; i++) {
 					if (instance->Qualifiers[i] == ObjectInfoId) {
-						AllSelectorObjectInstances.push_back(instance->Handle);
+						AllSelectorObjectInstances.push_back(instance);
 						break;
 					}
 				}
-			}
-			else if (instance->ObjectInfoHandle == ObjectInfoId) {
-				AllSelectorObjectInstances.push_back(instance->Handle);
+			} else if (instance->ObjectInfoHandle == ObjectInfoId) {
+				AllSelectorObjectInstances.push_back(instance);
 			}
 		}
-
-		std::sort(AllSelectorObjectInstances.begin(), AllSelectorObjectInstances.end());
+		std::sort(AllSelectorObjectInstances.begin(), AllSelectorObjectInstances.end(), 
+			[](ObjectInstance* a, ObjectInstance* b) { return a->Handle < b->Handle; });
 	}
 
 	// Called during a event to scope all objects that are relevant to the event
 	void Reset() {
-		SelectedInstances.clear();
-		for (const auto& handle : AllSelectorObjectInstances) {
-			if (AllInstances.find(handle) != AllInstances.end()) {
-				SelectedInstances.push_back(AllInstances.at(handle));
-			}
-			else {
-				// instance has been destroyed
-				RemoveInstance(handle);
-			}
+		for (auto* obj : SelectedInstances) {
+			obj->isSelected = false;
 		}
-
-		std::sort(SelectedInstances.begin(), SelectedInstances.end(), [](ObjectInstance* a, ObjectInstance* b) {
-			return a->Handle < b->Handle;
-		});
+		SelectedInstances = AllSelectorObjectInstances;
+		for (auto* obj : SelectedInstances) {
+			obj->isSelected = true;
+		}
+		IsDirty = false; 
 	}
 
 	// Called when an object is destroyed
 	void RemoveInstance(unsigned int handle) {
-		AllSelectorObjectInstances.erase(std::remove(AllSelectorObjectInstances.begin(), AllSelectorObjectInstances.end(), handle), AllSelectorObjectInstances.end());
+		auto it = std::find_if(AllSelectorObjectInstances.begin(), AllSelectorObjectInstances.end(),
+			[handle](ObjectInstance* p) { return p->Handle == handle; });
+		
+		if (it != AllSelectorObjectInstances.end()) {
+			(*it)->isSelected = false;
+			AllSelectorObjectInstances.erase(it);
+		}
+
+		auto itSel = std::find_if(SelectedInstances.begin(), SelectedInstances.end(),
+			[handle](ObjectInstance* p) { return p->Handle == handle; });
+		
+		if (itSel != SelectedInstances.end()) {
+			*itSel = SelectedInstances.back();
+			SelectedInstances.pop_back();
+			IsDirty = true;
+		}
 	}
 
 	void AddInstance(ObjectInstance* instance) {
-		if (std::find(AllSelectorObjectInstances.begin(), AllSelectorObjectInstances.end(), instance->Handle) == AllSelectorObjectInstances.end()) {
-			AllSelectorObjectInstances.push_back(instance->Handle);
+		if (!instance->isSelected) {
+			instance->isSelected = true;
+			SelectedInstances.push_back(instance);
+			IsDirty = true;
 			
-			std::sort(AllSelectorObjectInstances.begin(), AllSelectorObjectInstances.end());
+			auto it = std::lower_bound(AllSelectorObjectInstances.begin(), AllSelectorObjectInstances.end(), instance,
+				[](ObjectInstance* a, ObjectInstance* b) { return a->Handle < b->Handle; });
 			
-			if (std::find(SelectedInstances.begin(), SelectedInstances.end(), instance) == SelectedInstances.end()) {
-				SelectedInstances.push_back(instance);
-
-				std::sort(SelectedInstances.begin(), SelectedInstances.end(), [](ObjectInstance* a, ObjectInstance* b) {
-					return a->Handle < b->Handle;
-				});
+			if (it == AllSelectorObjectInstances.end() || (*it)->Handle != instance->Handle) {
+				AllSelectorObjectInstances.insert(it, instance);
 			}
 		}
 	}
@@ -85,72 +87,82 @@ public:
 	// Deselect an instance by index in the selected array
 	void DeselectAt(size_t index) {
 		if (index < SelectedInstances.size()) {
-			SelectedInstances.erase(SelectedInstances.begin() + index);
+			SelectedInstances[index]->isSelected = false;
+			SelectedInstances[index] = SelectedInstances.back();
+			SelectedInstances.pop_back();
+			IsDirty = true;
 		}
 	}
 
 	// Add an instance back to selection
 	void Select(ObjectInstance* instance) {
-		if (instance->ObjectInfoHandle == ObjectInfoId && 
-			std::find(SelectedInstances.begin(), SelectedInstances.end(), instance) == SelectedInstances.end()) {
+		if (instance->ObjectInfoHandle == ObjectInfoId && !instance->isSelected) {
+			instance->isSelected = true;
 			SelectedInstances.push_back(instance);
-
-			std::sort(SelectedInstances.begin(), SelectedInstances.end(), [](ObjectInstance* a, ObjectInstance* b) {
-				return a->Handle < b->Handle;
-			});
+			IsDirty = true;
 		}
 	}
 
 	void SelectOnly(ObjectInstance* instance) {
+		for (auto* obj : SelectedInstances) obj->isSelected = false;
 		SelectedInstances.clear();
+		
+		instance->isSelected = true;
 		SelectedInstances.push_back(instance);
+		IsDirty = false; 
 	}
 
 	void SelectRandom() {
 		if (!SelectedInstances.empty()) {
-			ObjectInstance* instance = SelectedInstances[Application::Instance().RandomRange(0, static_cast<short>(SelectedInstances.size() - 1))];
-			SelectedInstances.clear();
-			SelectedInstances.push_back(instance);
+			size_t randomIndex = Application::Instance().RandomRange(0, static_cast<short>(SelectedInstances.size() - 1));
+			ObjectInstance* instance = SelectedInstances[randomIndex];
+			SelectOnly(instance);
 		}
 	}
 
 	// Get number of selected instances
-	size_t Count() const {
-		return SelectedInstances.size();
-	}
-
+	size_t Count() const { return SelectedInstances.size(); }
+	
 	// Get number of all object instances in the selector
-	size_t Size() const {
-		return AllSelectorObjectInstances.size();
-	}
+	size_t Size() const { return AllSelectorObjectInstances.size(); }
 
 	// Access instance by index
 	ObjectInstance* At(size_t index) {
 		if (index < SelectedInstances.size()) {
+			EnsureSorted();
 			return SelectedInstances[index];
 		}
 		return nullptr;
 	}
 
 	// Iterator methods with improved signatures
-	iterator begin() { return SelectedInstances.begin(); }
+	iterator begin() { EnsureSorted(); return SelectedInstances.begin(); }
 	iterator end() { return SelectedInstances.end(); }
-	const_iterator begin() const { return SelectedInstances.begin(); }
+
+	const_iterator begin() const { 
+		const_cast<ObjectSelector*>(this)->EnsureSorted(); 
+		return SelectedInstances.begin(); 
+	}
 	const_iterator end() const { return SelectedInstances.end(); }
-	
-	// Additional iterator methods for better compatibility
-	const_iterator cbegin() const { return SelectedInstances.cbegin(); }
-	const_iterator cend() const { return SelectedInstances.cend(); }
 
 	// Friend declaration for the iterator
 	friend class ObjectIterator;
 
 private:
+	void EnsureSorted() {
+		if (IsDirty) {
+			std::sort(SelectedInstances.begin(), SelectedInstances.end(), 
+				[](ObjectInstance* a, ObjectInstance* b) { return a->Handle < b->Handle; });
+			IsDirty = false;
+		}
+	}
+
 	const std::unordered_map<unsigned int, ObjectInstance*>& AllInstances;
-	std::vector<ObjectInstance*> SelectedInstances;
-	std::vector<int> AllSelectorObjectInstances; // list of all object instance handles for this selector
+	std::vector<ObjectInstance*> SelectedInstances;	  
+	std::vector<ObjectInstance*> AllSelectorObjectInstances; // list of all object instance handles for this selector
 	unsigned int ObjectInfoId;
-	bool IsQualifier = false;
+	bool IsQualifier;
+	bool IsDirty; 
 };
 
 // Add ObjectIterator class to support the requested iteration pattern
@@ -158,46 +170,36 @@ class ObjectIterator {
 public:
 	ObjectIterator(ObjectSelector& selector) 
 		: selector_(selector), currentIndex_(0), skipIncrement_(false) {
+		selector_.EnsureSorted();
 	}
 	
 	// Check if we've reached the end of iteration
-	bool end() const {
-		return currentIndex_ >= selector_.SelectedInstances.size();
-	}
+	bool end() const { return currentIndex_ >= selector_.SelectedInstances.size(); }
 	
 	// Increment operator to move to next object
 	ObjectIterator& operator++() {
 		if (!end()) {
 			// Only increment if we're not supposed to skip
-			if (!skipIncrement_) {
-				currentIndex_++;
-			} else {
-				// Reset the flag after using it
-				skipIncrement_ = false;
-			}
+			if (!skipIncrement_) currentIndex_++;
+			else skipIncrement_ = false; // Reset the flag after using it
 		}
 		return *this;
 	}
 	
 	// Dereference operator to get the current object
 	ObjectInstance* operator*() {
-		if (!end()) {
-			return selector_.SelectedInstances[currentIndex_];
-		}
-		return nullptr;
+		return end() ? nullptr : selector_.SelectedInstances[currentIndex_];
 	}
 	
 	// Deselect the current object during iteration
 	void deselect() {
 		if (!end()) {
-			selector_.SelectedInstances.erase(selector_.SelectedInstances.begin() + currentIndex_);
+			selector_.DeselectAt(currentIndex_);
 			skipIncrement_ = true;
 		}
 	}
 
-	size_t index() const {
-		return currentIndex_;
-	}
+	size_t index() const { return currentIndex_; }
 	
 private:
 	ObjectSelector& selector_;
