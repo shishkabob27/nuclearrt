5c650a11ec67796d4cd238574b4c6b40a5ef8cc7
programminglaboratorys <programming.laboratorys@gmail.com>
2026-04-01 08:00:10 +0300

    fix: object instance layout

--- runtime/include/ObjectInstance.h
+++ runtime/include/ObjectInstance.h
@@ -12,56 +12,58 @@ public:
     ObjectInstance(unsigned int objectInfoHandle, int type, std::string name)
         : ObjectInfoHandle(objectInfoHandle), Type(type), Name(name) {}
     virtual ~ObjectInstance() = default;
-	
-	unsigned int Handle = 0;
-	unsigned int Type = 0;
-	unsigned int ObjectInfoHandle = 0;
-	std::string Name = "";
-	
-	int X = 0;
-	int Y = 0;
-
+    
+    std::string Name = "";
+    std::vector<short> Qualifiers = { -1, -1, -1, -1, -1, -1, -1, -1 };
+    
+    unsigned int Handle = 0;
+    unsigned int Type = 0;
+    unsigned int ObjectInfoHandle = 0;
 	unsigned int Layer = 0;
-
-	short InstanceValue = 0;
-	std::vector<short> Qualifiers = { -1, -1, -1, -1, -1, -1, -1, -1 };
-
-	bool global = false;
-
-	int RGBCoefficient = 0xFFFFFF;
-	int Effect = 0;
-
-	unsigned char GetEffectParameter() const {
-		return EffectParameter;
-	}
-	
-	void SetEffectParameter(int effectParameter) {
-		EffectParameter = static_cast<unsigned char>(std::clamp(effectParameter, 0, 255));
-	}
-	
-	unsigned int GetAngle() const {
-		return Angle;
-	}
-
-	void SetAngle(int angle) {
-		//limit to 0-359 degrees
-		while (angle < 0) {
-			angle += 360;
-		}
-		while (angle >= 360) {
-			angle -= 360;
-		}
-		
-		Angle = angle;
-	}
-
-	virtual ObjectGlobalData* CreateGlobalData() { return nullptr; };
-	virtual void ApplyGlobalData(ObjectGlobalData* globalData) { };
-
-	virtual std::vector<unsigned int> GetImagesUsed() { return std::vector<unsigned int>(); };
-	virtual std::vector<unsigned int> GetFontsUsed() { return std::vector<unsigned int>(); };
-
 private:
-	unsigned int Angle = 0;
-	unsigned char EffectParameter = 0;
+    unsigned int Angle = 0;
+public:
+
+	int X = 0;
+    int Y = 0;
+    int RGBCoefficient = 0xFFFFFF;
+    int Effect = 0;
+    
+    short InstanceValue = 0;
+    
+    bool global = false;
+private:
+    unsigned char EffectParameter = 0;
+public:
+
+
+    unsigned char GetEffectParameter() const {
+        return EffectParameter;
+    }
+    
+    void SetEffectParameter(int effectParameter) {
+        EffectParameter = static_cast<unsigned char>(std::clamp(effectParameter, 0, 255));
+    }
+    
+    unsigned int GetAngle() const {
+        return Angle;
+    }
+
+    void SetAngle(int angle) {
+        while (angle < 0) {
+            angle += 360;
+        }
+        while (angle >= 360) {
+            angle -= 360;
+        }
+        
+        Angle = angle;
+    }
+
+    virtual ObjectGlobalData* CreateGlobalData() { return nullptr; };
+    virtual void ApplyGlobalData(ObjectGlobalData* globalData) { };
+
+    virtual std::vector<unsigned int> GetImagesUsed() { return std::vector<unsigned int>(); };
+    virtual std::vector<unsigned int> GetFontsUsed() { return std::vector<unsigned int>(); };
+
 };
